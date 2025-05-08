5d665927f595b81d841a1604e695fd97891a1fb3
XenorSor <biro.tamas@stud.uni-obuda.hu>
2025-05-08 23:58:36 +0200

    init

---
+++ src/ros2_webots/ros2_webots/car_driver.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros2_webots.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/ros2_webots/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2_webots'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='xenor',
+    maintainer_email='xenor@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'car_driver = ros2_webots.car_driver:main'
+        ],
+    },
+)
