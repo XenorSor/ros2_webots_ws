35474d3b8fdce6cc38640a9949aefd7af8ae8e55
XenorSor <biro.tamas@stud.uni-obuda.hu>
2025-05-13 18:32:05 +0200

    test

--- install/ros2_webots/lib/python3.10/site-packages/ros2_webots/obstacle_avoider.py
+++ install/ros2_webots/lib/python3.10/site-packages/ros2_webots/obstacle_avoider.py
@@ -14,21 +14,36 @@ class ObstacleAvoider(Node):
         self.create_subscription(Range, 'left_sensor', self.__left_sensor_callback, 1)
         self.create_subscription(Range, 'right_sensor', self.__right_sensor_callback, 1)
 
+        self.__left_sensor_value = MAX_RANGE
+        self.__right_sensor_value = MAX_RANGE
+
+        self.create_timer(0.05, self.control_loop)
+
     def __left_sensor_callback(self, message):
         self.__left_sensor_value = message.range
     
     def __right_sensor_callback(self, message):
         self.__right_sensor_value = message.range
 
+    
+    def control_loop(self):
         command_message = Twist()
 
-        command_message.linear.x = 0.1
-
-        if self.__left_sensor_value < 0.8 * MAX_RANGE or self.__right_sensor_value < 0.8 * MAX_RANGE:
+        if self.__left_sensor_value < 0.1 or self.__right_sensor_value < 0.1:
             command_message.angular.z = -2.0
+            command_message.linear.x = 0.02
+        elif self.__left_sensor_value < 0.1:
+            command_message.angular.z = -1.0
+            command_message.linear.x = 0.05
+        elif self.__right_sensor_value < 0.1:
+            command_message.angular.z = 1.0
+            command_message.linear.x = 0.05
+        else:
+            command_message.angular.z = 0.0
+            command_message.linear.x = 0.1
         
         self.__publisher.publish(command_message)
-    
+
 def main(args=None):
     rclpy.init(args=args)
     avoider = ObstacleAvoider()
