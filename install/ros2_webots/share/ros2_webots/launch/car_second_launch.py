358fc72d08279df29a1770e229b8f0239bb35226
XenorSor <biro.tamas@stud.uni-obuda.hu>
2025-05-13 17:45:56 +0200

    obstacle_avoider

--- install/ros2_webots/share/ros2_webots/launch/car_second_launch.py
+++ install/ros2_webots/share/ros2_webots/launch/car_second_launch.py
@@ -5,10 +5,11 @@ from ament_index_python.packages import get_package_share_directory
 import launch.event_handlers
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
+from launch_ros.actions import Node
 
 def generate_launch_description():
     package_dir = get_package_share_directory('ros2_webots')
-    robot_description_path = os.path.join(package_dir, 'resource', 'car.urdf')
+    robot_description_path = os.path.join(package_dir, 'resource', 'car_with_sensors.urdf')
 
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'car_2.wbt')
@@ -22,9 +23,15 @@ def generate_launch_description():
         respawn=True
     )
 
+    obstacle_avoider = Node(
+        package='ros2_webots',
+        executable='obstacle_avoider'
+    )
+
     return LaunchDescription([
         webots,
         my_robot_driver,
+        obstacle_avoider,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
--- src/ros2_webots/setup.py
+++ src/ros2_webots/setup.py
@@ -24,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            #'car_driver = ros2_webots.car_driver:main'
+            'obstacle_avoider = ros2_webots.obstacle_avoider:main'
         ],
     },
 )
